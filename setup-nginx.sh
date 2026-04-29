765ba69ba3b6cd751e08add23772608c1dea30c6
ECClabs <eccilabs@gmail.com>
2026-04-29 11:25:24 +0530

    Added setup nginx file

---
+++ setup-nginx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install nginx
+apt-get -y update
+apt -y install nginx
+
+# Remove the default page
+rm /var/www/html/index.nginx-debian.html
+
+# Create the default page for the region
+wget https://raw.githubusercontent.com/ECClabs/ecc-distribute-load-with-traffic-manager/master/index.$1.html -O /var/www/html/index.html
